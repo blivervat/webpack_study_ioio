c28d1542bec83d65b4bb065e98405c09740e66e1
王京 <blivervat@gmail.com>
2020-10-29 18:25:55 +0800

    feat： 项目初始化

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isDev = process.env.NODE_ENV === 'development';
+module.exports = {
+    mode: isDev ? 'development' : 'production',
+    entry: './main.js', // 入口文件
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'my-first-webpack.bundle.js' // 出口文件
+    },
+    // 使用loader处理非js文件, 让webpack可以处理这些文件
+    module: {
+        /*          
+         * test 属性，用于标识出应该被对应的 loader 进行转换的某个或某些文件。
+         * use 属性，表示进行转换时，应该使用哪个 loader。
+         */
+        rules: [
+            {
+                test: /\.jsx?$/,
+                use: ['babel-loader'],
+                exclude: /node_modules/ //排除 node_modules 目录
+            }
+        ]
+    },
+    // plugins用于执行范围更广的任务,从打包优化和压缩，一直到重新定义环境中的变量
+    // 插件目的在于解决 loader 无法实现的其他事。
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: 'index.html', //打包后的文件名
+            minify: {
+                removeAttributeQuotes: false, //是否删除属性的双引号
+                collapseWhitespace: false, //是否折叠空白
+            },
+            // hash: true //是否加上hash，默认是 false
+        })
+    ],
+    devServer: {
+        port: '8080', //默认是8080
+        quiet: false, //默认不启用
+        inline: true, //默认开启 inline 模式，如果设置为false,开启 iframe 模式
+        stats: "errors-only", //终端仅打印 error
+        overlay: false, //默认不启用
+        clientLogLevel: "silent", //日志等级
+        compress: true //是否启用 gzip 压缩
+    }
+}
